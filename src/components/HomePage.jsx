aa132b09a35c76b05ab45ae7269383c9dcd3469f
Kushagra <kushagrasingh1100@gmail.com>
2025-06-14 01:06:24 +0530

    fixed theme, added homepage carousel and fixed work images height

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,9 +1,61 @@
+import { useEffect, useState } from "react";
 import "./HomePage.css";
+import Carousel1 from "../assets/marketing1.jpg"
+import Carousel2 from "../assets/marketing2.jpg"
+import Carousel3 from "../assets/marketing3.jpg"
 
 function HomePage() {
+  const [activeTab, setActiveTab] = useState(1);
+
+  const carouselTabs = [
+    {
+      id: 1,
+      title: "Listen to Your Customers",
+      image: Carousel1,
+    },
+    {
+      id: 2,
+      title: "Custom Metrics for Smarter Decisions",
+      image: Carousel2,
+    },
+    {
+      id: 3,
+      title: "Actionable Recommendations",
+      image: Carousel3,
+    },
+  ];
+
+  const length = carouselTabs.length;
+  
+    const handleNextTab = (tabId) => {
+      if (tabId === length) {
+        setActiveTab(1);
+      } else {
+        setActiveTab(tabId + 1);
+      }
+    };
+  
+    let interval;
+  
+    useEffect(() => {
+      interval = setInterval(() => {
+        handleNextTab(activeTab);
+      }, 5000);
+  
+      return () => {
+        if (interval) {
+          clearInterval(interval);
+        }
+      };
+    }, [activeTab]);
+  
+    const activeItem =
+      carouselTabs.find((item) => item.id === activeTab) || carouselTabs[0];
+
   return (
     <div className="home-page">
       <div className="home-carousel">
+        <img src={activeItem.image} alt="" />
         <div className="home-text">
           <h3>Meet your next digital agency</h3>
           <h1>
