8372c72974e93b3d251177f07fc8720cdc42203b
Achintya <achintyasharma234@gmail.com>
2025-06-23 17:01:00 +0530

    Footer Updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,77 +1,40 @@
 import React from "react";
 import "./Footer.css";
-import { FaYoutube } from "react-icons/fa6";
 import { CiInstagram } from "react-icons/ci";
 import { FaLinkedin } from "react-icons/fa";
 import { FaWhatsappSquare } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <footer className="footer" id="footer">
-      <div className="footer-content">
-        <div className="footer-left">
-          <div className="footer-logo">
-            <img src="/logo.png" alt="" />
-            <div className="logo-text">Ignite digital</div>
-            <div className="company-description">
-              Web Development & Software Company
-            </div>
-          </div>
-        </div>
-
-        <div className="footer-addresses">
-          <div className="address india">
-            <h3>India</h3>
-            <p>SCO-4, 1st Floor, Omaxe Royal</p>
-            <p>Residency, Ludhiana, 142022</p>
-            <a href="tel:+919814520845" className="phone-link">
-              +91 9814520845
-            </a>
-            <a href="https://maps.google.com" className="map-link">
-              View on Google Maps
-            </a>
-          </div>
-
-          <div className="address usa">
-            <h3>USA</h3>
-            <p>513 Ranch Trl, Irving, TX 75063</p>
-            <a href="tel:+1(929)393-0969" className="phone-link">
-              +1 (929) 393-0969
-            </a>
-            <p>48870 Eagle View Terrace, Fremont</p>
-            <p>CA 94539</p>
-          </div>
-        </div>
-
-        <div className="footer-newsletter">
-          <h3>Sign up to our newsletter</h3>
-          <form className="newsletter-form">
-            <input type="text" placeholder="Name" className="form-input" />
-            <input
-              type="email"
-              placeholder="Email Address"
-              className="form-input"
-            />
-            <button type="submit" className="subscribe-btn">
-              Subscribe
-            </button>
-          </form>
-          <p className="privacy-notice">
-            Digital Kangaroos needs this information you give to contact you
-            about our products and services. You may unsubscribe at any time.
-            For more information please review our{" "}
-            <a href="/privacy-policy" className="policy-link">
-              privacy policy
-            </a>
-            .
-          </p>
-        </div>
-      </div>
-
       <div className="footer-divider"></div>
 
       <div className="footer-bottom">
+        <div className="footer-a1">
+          <Link to="/">
+          <div className="logo-bottom">
+            <img src="/IGNITE.png" alt="Ignite Digitals Logo" />
+            <div className="logo-text">
+              <h1>IGNITE DIGITALS</h1>
+              <div className="company-description">
+                  Web Development & Software Company
+              </div>
+            </div>
+          </div>
+          </Link>
+          <div className="footer-addresses">
+            <div className="address india">
+              <h3>India</h3>
+              <p>SCO-4, 1st Floor, Omaxe Royal</p>
+              <p>Residency, Ludhiana, 142022</p>
+              <a href="tel:+919814520845" className="phone-link">
+                +91 9814520845
+              </a>
+            </div>
+          </div>
+        </div>
         <div className="social-links">
           <a href="#" className="social-icon facebook">
             <FaFacebook />
@@ -85,30 +48,15 @@ const Footer = () => {
           <a href="#" className="social-icon whatsapp">
             <FaWhatsappSquare />
           </a>
-          <a href="#" className="social-icon youtube">
-            <FaYoutube />
-          </a>
         </div>
-
+{/* 
         <nav className="footer-nav">
           <a href="/about">About</a>
           <a href="/services">Services</a>
-          <a href="/work">Work</a>
-          <a href="/press-releases">Press releases</a>
-          <a href="/blogs">Blogs</a>
-          <a href="/faqs">FAQs</a>
-          <a href="/contact">Contact</a>
-          <a href="/privacy-policy">Privacy policy</a>
-          <a href="/terms">Terms & conditions</a>
-        </nav>
+        </nav> */}
 
         <div className="footer-copyright">
-          <p>© Digital Kangaroos | All Rights Reserved 2023</p>
-          <div className="footer-links">
-            <a href="/dk-company-projects">DK Company Projects</a>
-            <a href="/cart-potato">Cart Potato</a>
-            <a href="/dk-school">DK School</a>
-          </div>
+          <p>© Ignite Digitals | All Rights Reserved {new Date().getFullYear()}</p>
         </div>
       </div>
     </footer>
